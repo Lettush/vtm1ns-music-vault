c2b6d5a1f7911180d2723359307cbce74ab8482b
Jared <jaredneilhortaleza11@gmail.com>
2024-03-31 21:23:37 +0800

    fixed responsiveness

--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -3,35 +3,37 @@ import YouTube from "../sections/YouTube";
 
 const Music = () => {
   const [latest, setLatest] = useState("");
-  useEffect(() => {
-    fetch(
-      "https://www.googleapis.com/youtube/v3/search?key=AIzaSyB05ZW_6lgBCtA1UtoRKP0uSG52HWpWtsI&channelId=UC8H9civC3067vONXbBNDmrw&part=id&order=date&maxResults=1"
-    )
-      .then((res) => res.json())
-      .then((data) => setLatest(data.items[0].id.videoId));
-  }, []);
+
+  /* Youtube API */
+  // useEffect(() => {
+  //   fetch(
+  //     "https://www.googleapis.com/youtube/v3/search?key=AIzaSyB05ZW_6lgBCtA1UtoRKP0uSG52HWpWtsI&channelId=UC8H9civC3067vONXbBNDmrw&part=id&order=date&maxResults=1"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setLatest(data.items[0].id.videoId));
+  // }, []);
   return (
     <>
-      <h1 className="section-heading">Music</h1>
       <div className="spotify column-layout">
         <div>
           <h1>Spotify</h1>
         </div>
         <iframe
           title="spotify-frame"
-          style={{ borderRadius: "12px" }}
-          src="https://open.spotify.com/embed/artist/6FSaNQlUJjC2X9hDQNFzwY?utm_source=generator&theme=0"
+          src="https://open.spotify.com/embed/artist/6FSaNQlUJjC2X9hDQNFzwY?utm_source=generator"
+          style={{ borderRadius: "10px", border: "none" }}
           width="100%"
           height="352"
-          frameBorder="0"
-          allowFullScreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         ></iframe>
       </div>
       <hr className="section-line" />
       <div className="youtube column-layout">
-        <YouTube link={`https://www.youtube.com/watch?v=${latest}`} className="vid"/>
+        <YouTube
+          link={`https://www.youtube.com/watch?v=n7a9t9NYBto`}
+          className="vid"
+        />
         <div className="text">
           <h1>YouTube</h1>
         </div>
--- src/components/sections/YouTube.js
+++ src/components/sections/YouTube.js
@@ -2,7 +2,7 @@ import ReactPlayer from "react-player/youtube";
 
 const YouTube = ({link}) => {
     return (
-        <ReactPlayer url={link} />
+        <ReactPlayer url={link} height={""}/>
     )
 }
 
