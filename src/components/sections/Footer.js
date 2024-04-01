f660cf0b4248d9d533683fec1bf1887aaaf680ee
Jared <jaredneilhortaleza11@gmail.com>
2024-04-01 11:09:54 +0800

    added twitter to footer

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,6 +1,6 @@
 import SoundCloud from "../../assets/images/sc.png"
 import Spotify from "../../assets/images/spotify.webp"
-import { YouTube } from "@mui/icons-material";
+import { YouTube, Twitter } from "@mui/icons-material";
 
 const Footer = () => {
   return (
@@ -8,6 +8,7 @@ const Footer = () => {
       <div>
         <a href="https://soundcloud.com/vtm1ns" target="_blank" rel="noreferrer"><img src={SoundCloud} alt="SC" /></a>
         <a href="https://youtube.com/c/vtm1ns" target="_blank" rel="noreferrer"><YouTube fontSize="large"/></a>
+        <a href="https://twitter.com/vtm1ns" target="_blank" rel="noreferrer"><Twitter fontSize="large"/></a>
         <a href="https://open.spotify.com/artist/6FSaNQlUJjC2X9hDQNFzwY" target="_blank" rel="noreferrer"><img src={Spotify} alt="SC" /></a>
       </div>
       <hr />
