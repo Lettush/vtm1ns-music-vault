6446a22db630b5412d8a69f3516e798e915113d4
Jared <jaredneilhortaleza11@gmail.com>
2024-04-02 18:21:27 +0800

    added more songs

--- src/components/pages/Music.js
+++ src/components/pages/Music.js
@@ -1,5 +1,10 @@
 // import { useEffect, useState } from "react";
 import YouTube from "../sections/YouTube";
+import { Link } from "react-router-dom";
+import Default from "../../assets/images/default.jpg";
+import Frog from "../../assets/images/frog.jpg";
+import Finn from "../../assets/images/finn.jpg";
+import Lofi from "../../assets/images/lofi.jpg";
 
 const Music = () => {
   // const [latest, setLatest] = useState("");
@@ -39,6 +44,41 @@ const Music = () => {
           <h1>YouTube</h1>
           <p>2,000+ Subscribers</p>
         </div>
+      </div>
+      <h1 className="section-heading">
+        <span> Samples </span>
+      </h1>
+      <div className="samples">
+        <Link to="/music/1">
+          <img
+            src="https://i1.sndcdn.com/artworks-FWKQmghCmbcybdZO-XiVEPA-t500x500.jpg"
+            alt="Audio 1"
+          />
+          <p>Jus Hav Fun Vol. 2 Remix</p>
+        </Link>
+        <Link to="/music/2">
+          <img
+            src="https://m.media-amazon.com/images/I/51fsb4sNp3L._UXNaN_FMjpg_QL85_.jpg"
+            alt="Audio 2"
+          />
+          <p>Blessed By Swords (Reprise)</p>
+        </Link>
+        <Link to="/music/3">
+          <img src={Default} alt="Audio 3" />
+          <p>Grenada (Unreleased)</p>
+        </Link>
+        <Link to="/music/4">
+          <img src={Frog} alt="Audio 4" />
+          <p>Everlasting</p>
+        </Link>
+        <Link to="/music/5">
+          <img src={Lofi} alt="Audio 5" />
+          <p>懐かしい</p>
+        </Link>
+        <Link to="/music/6">
+          <img src={Finn} alt="Audio 6" />
+          <p>Corrupted Hero (Remastered)</p>
+        </Link>
       </div>
     </>
   );
--- src/components/pages/MusicDetail.js
+++ src/components/pages/MusicDetail.js
@@ -2,6 +2,9 @@ import AudioPlayer from "../AudioPlayer";
 import Audio1 from "../../assets/audio/1.mp3";
 import Audio2 from "../../assets/audio/2.mp3";
 import Audio3 from "../../assets/audio/3.ogg";
+import Audio4 from "../../assets/audio/4.wav";
+import Audio5 from "../../assets/audio/5.wav";
+import Audio6 from "../../assets/audio/6.mp3";
 import { useParams } from "react-router-dom";
 
 const MusicDetail = () => {
@@ -14,6 +17,12 @@ const MusicDetail = () => {
       return <AudioPlayer audioFile={Audio2} name="Blessed By Swords (Reprise)" />;
     case "3":
       return <AudioPlayer audioFile={Audio3} name="Grenada (Unreleased)" />;
+    case "4":
+      return <AudioPlayer audioFile={Audio4} name="Everlasting" />;
+    case "5":
+      return <AudioPlayer audioFile={Audio5} name="懐かしい" />;
+    case "6":
+      return <AudioPlayer audioFile={Audio6} name="Corrupted Hero (Remastered)" />;
     default:
       break;
   }
